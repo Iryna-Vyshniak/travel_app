def7845f090c3824f67f0c356fb53f6c08a8f757
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-10-21 01:39:45 +0300

    Create and complete Footer columns component

---
+++ components/Footer/FooterColumn.tsx
@@ -0,0 +1,15 @@
+type FooterColumnProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
+  return (
+    <div className='flex flex-col gap-5'>
+      <h4 className='bold-18 whitespace-nowrap'>{title}</h4>
+      {children}
+    </div>
+  );
+};
+
+export default FooterColumn;
