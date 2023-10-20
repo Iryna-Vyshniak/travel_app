4346539c8a9af39408bdcac9a34cc05d189252eb
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-10-21 01:37:05 +0300

    add content

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,8 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './section/**/*.{html,js,jsx,tsx}',
+    './styles/**/*.{js,jsx}',
   ],
   theme: {
     extend: {
