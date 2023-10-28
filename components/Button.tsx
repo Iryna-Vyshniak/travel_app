5b197b457647a4d9976de19fe87c1e02d96428c3
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-10-28 23:57:34 +0300

    add field onclick

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,11 +6,16 @@ type ButtonProps = {
   icon?: string;
   variant: string;
   full?: boolean;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
-const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full, onClick }: ButtonProps) => {
   return (
-    <button className={`flexCenter gap-3 rounded-full ${variant} ${full && 'w-full'}`} type={type}>
+    <button
+      className={`flexCenter gap-3 rounded-full ${variant} ${full && 'w-full'}`}
+      type={type}
+      onClick={onClick}
+    >
       {icon && <Image src={icon} alt={title} width={16} height={16} loading='lazy' />}
       <label className='text-base whitespace-nowrap cursor-pointer'>{title}</label>
     </button>
