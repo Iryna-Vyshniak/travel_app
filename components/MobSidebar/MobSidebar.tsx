626d6de37ba1d92484843720b3ae48234baf9ead
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-10-29 03:34:16 +0300

    Implement mobile sidebar with MenuToggle for navigation

---
+++ components/MobSidebar/MobSidebar.tsx
@@ -0,0 +1,32 @@
+'use client';
+import { useRef } from 'react';
+import { motion, sync, useCycle } from 'framer-motion';
+import { useDimensions } from '@/shared/hooks/useDimensions';
+import { MenuToggle } from './MenuToggle';
+import { Navigation } from './Navigation';
+import { sidebar } from '@/shared/utils.ts/motion';
+
+const MobSidebar = () => {
+  const [isOpen, toggleOpen] = useCycle(false, true);
+  const containerRef = useRef(null);
+  const { height } = useDimensions(containerRef);
+
+  return (
+    <motion.nav
+      initial={false}
+      animate={isOpen ? 'open' : 'closed'}
+      custom={height}
+      ref={containerRef}
+      className='absolute top-0 right-0 bottom-0 z-40 w-[50vw] lg:hidden'
+    >
+      <motion.div
+        className='absolute top-0 right-0 bottom-0 w-[50vw] min-h-screen bg-white shadow-lg shadow-slate-800'
+        variants={sidebar}
+      />
+      <Navigation />
+      <MenuToggle toggle={() => toggleOpen()} />
+    </motion.nav>
+  );
+};
+
+export default MobSidebar;
