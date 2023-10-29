66540d91e519bbe49bcc62726c5af1c1eea1b232
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-10-29 03:30:25 +0300

    Add intersection observer for Section component animation

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,3 +1,7 @@
+'use client';
+import { useRef } from 'react';
+import { useInView } from 'framer-motion';
+
 interface Props {
   id?: string;
   variant?: string;
@@ -5,12 +9,21 @@ interface Props {
 }
 
 const Section = ({ id, variant, children }: Props) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
   return (
     <section
       id={id}
+      ref={ref}
       className={
         !variant ? 'relative flexCenter flex-col' : `${variant} relative flexCenter flex-col`
       }
+      style={{
+        transform: isInView ? 'none' : 'translateY(200px)',
+        opacity: isInView ? 1 : 0,
+        transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s',
+      }}
     >
       {children}
     </section>
