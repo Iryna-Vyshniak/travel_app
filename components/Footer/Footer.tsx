cb39bd6b634baee2b7b1539e23dd6fcdb3c66e8f
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-11-02 23:12:47 +0200

    complete with UI

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,16 +2,16 @@ import Link from 'next/link';
 
 import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '@/constants';
 import FooterColumn from './FooterColumn';
-import Logo from '../Logo';
+import Logo from '../Header/Logo';
 import Socials from '../Socials';
 import Contacts from '../Contacts';
+import Container from '../Container';
 
 const Footer = () => {
   return (
-    <footer className='flexCenter mt-24'>
-      <div className='padding-container max-container flexCenter w-full flex-col gap-14'>
-        <div className='w-full h-[722px] bg-forest bg-contain bg-no-repeat' />
-        <div className='flex flex-col items-start justify-center gap-[10%] md:flex-row'>
+    <footer className='w-screen overflow-hidden bg-cover bg-fixed bg-center bg-no-repeat bg-activity'>
+      <Container variant='padding-container max-container flexCenter w-full flex-col gap-2 py-8'>
+        <div className='flex flex-col items-start justify-center gap-[10%] md:flex-row py-2'>
           <Logo />
 
           <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
@@ -19,7 +19,7 @@ const Footer = () => {
               <FooterColumn title={columns.title} key={columns.title}>
                 <ul className='regular-14 flex flex-col gap-4 text-gray-30'>
                   {columns.links.map((link) => (
-                    <Link href='/' key={link}>
+                    <Link href='/' key={link} className='text-white'>
                       {link}
                     </Link>
                   ))}
@@ -39,11 +39,11 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className='border bg-gray-20' />
-        <p className='regular-14 w-full text-center text-gray-30'>
+        <div className='border bg-gray-200 w-full' />
+        <p className='py-2 regular-14 w-full text-center text-white'>
           2023 PonteChianale | All rights reserved
         </p>
-      </div>
+      </Container>
     </footer>
   );
 };
--- components/Footer/FooterColumn.tsx
+++ components/Footer/FooterColumn.tsx
@@ -6,7 +6,7 @@ type FooterColumnProps = {
 const FooterColumn = ({ title, children }: FooterColumnProps) => {
   return (
     <div className='flex flex-col gap-5'>
-      <h4 className='bold-18 whitespace-nowrap'>{title}</h4>
+      <h4 className='bold-18 whitespace-nowrap text-white'>{title}</h4>
       {children}
     </div>
   );
