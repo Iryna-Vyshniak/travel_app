ec11595ff28350509ecc6d0a9e71573ad6696234
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-11-02 23:03:45 +0200

    Implement header component

---
+++ components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import Navbar from './Navbar';
+
+const Header = () => {
+  return (
+    <header className='sticky left-0 right-0 top-0 z-30  h-[88px] shadow-lg shadow-orange-90 overflow-hidden bg-cover bg-center bg-repeat bg-texture'>
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
