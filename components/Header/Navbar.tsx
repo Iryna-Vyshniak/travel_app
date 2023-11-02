3e2256a28449b092c64136c4b864753cc63f7910
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-11-02 23:03:08 +0200

    Implement navbar component

---
+++ components/Header/Navbar.tsx
@@ -0,0 +1,34 @@
+import Logo from './Logo';
+import Socials from '../Socials';
+import Nav from './Nav';
+import Button from '../Button';
+import Contacts from '../Contacts';
+import MobSidebar from '../MobSidebar/MobSidebar';
+
+const Navbar = () => {
+  return (
+    <nav className='relative flexBetween gap-2 max-container padding-container py-2'>
+      <div className='flexCenter flex-row-reverse 2xl:flex-row'>
+        <div className='hidden h-full gap-8 xl:flexCenter'>
+          <Socials text='nav' />
+          <Contacts text='nav' />
+        </div>
+        <div className='ml-0 mr-5 2xl:hidden'>
+          <Logo />
+        </div>
+      </div>
+      <div className='hidden 2xl:inline-block'>
+        <Logo />
+      </div>
+
+      <Nav />
+      <div className='lg:flexCenter hidden'>
+        <Button type='button' title='Login' icon='/user.svg' variant='btn_green' />
+      </div>
+
+      <MobSidebar />
+    </nav>
+  );
+};
+
+export default Navbar;
