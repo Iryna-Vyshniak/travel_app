8c8a8dbb60f8cadde4295e7ed1cbeea80919a371
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-11-05 22:22:53 +0200

    Complete homepage displaying booking options

--- app/camping/page.tsx
+++ app/camping/page.tsx
@@ -1,13 +1,22 @@
-import Booking from '@/components/Camp/Booking';
+import CampCard from '@/components/Camp/CampCard';
 import Container from '@/components/Container';
 import PageWrapper from '@/components/PageWrapper';
-import Section from '@/components/Section';
+import Title from '@/components/Title/Title';
+import { BOOKING } from '@/constants';
 
 const Home = () => {
   return (
     <PageWrapper>
       <Container>
-        <Booking />
+        <Container>
+          <p className='self-start uppercase regular-18 mb-3 text-green-60'>Booking</p>
+          <Title text='Book your dream vacation now' variant='self-start' />
+          <ul className='cards-list'>
+            {BOOKING.map((data) => (
+              <CampCard key={data.id} data={data} />
+            ))}
+          </ul>
+        </Container>
       </Container>
     </PageWrapper>
   );
