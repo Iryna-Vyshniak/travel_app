31c910da9afad1012c6841d9f6edabb58566d960
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-11-05 22:08:53 +0200

    add attribution and url for mapping

--- constants/index.ts
+++ constants/index.ts
@@ -401,7 +401,7 @@ export const BOOKING = [
         "stars": 4.88,
         "reviews": 86,
         "tags": ['#vacation', '#lakeside', '#travel', '#camping'],
-        "position": [44.97325084074172, -85.46561755429681]
+        "position": [44.96942730685429, -85.47348807140315]
     },
     {
         "name": "Canyon Adventure Camp",
@@ -480,4 +480,10 @@ export const ABOUT = [
     }
 ]
 
+// mapping
+
+export const attribution =
+    'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>';
+export const url =
+    'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibGFkeWJvc3N0b3JuIiwiYSI6ImNsb2tsdnYwcjI2bmUycW1lNGY1M3Jyd3MifQ.Qm7B3j1i4d6z4vjaCimaEQ';
 
