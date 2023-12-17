f44eddebf5f1cad6b9caa1699f71ce356db6a691
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2023-12-17 21:35:23 +0200

    added Features and Contacts components

--- components/Contacts.tsx
+++ components/Contacts.tsx
@@ -11,13 +11,17 @@ const Contacts = ({ text }: Props) => {
       {FOOTER_CONTACT_INFO.links.map((link) => (
         <Link href={link.href} key={link.label} className='flex gap-4 md:flex-col lg:flex-row'>
           {text ? (
-            <p className='regular-14 flexCenter cursor-pointer pb-1.5 text-white hover:font-bold hover:text-green-50 hover:drop-shadow-[0_1px_0.5px_rgba(0,0,0,1)] transition-all ease-in-out duration-200'>
+            <p className='regular-14 flexCenter cursor-pointer pb-1.5 text-white hover:text-green-50 hover:drop-shadow-[0_1px_0.5px_rgba(0,0,0,1)] transition-all ease-in-out duration-200'>
               {link.value}
             </p>
           ) : (
             <>
-              <p className='whitespace-nowrap'>{link.label}:</p>
-              <p className='medium-14 whitespace-nowrap text-blue-70'>{link.value}</p>
+              <p className='whitespace-nowrap regular-14 cursor-pointer pb-1.5 text-white hover:text-green-50 hover:drop-shadow-[0_1px_0.5px_rgba(0,0,0,1)] transition-all ease-in-out duration-200'>
+                {link.label}:
+              </p>
+              <p className='medium-14 whitespace-nowrap text-blue-100 hover:text-green-50 hover:drop-shadow-[0_1px_0.5px_rgba(0,0,0,1)] transition-all ease-in-out duration-200'>
+                {link.value}
+              </p>
             </>
           )}
         </Link>
