a67e60e4be968b5b0a7d6cd85c52c49d38570476
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2023-12-18 23:26:37 +0200

    complete button props

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,21 +2,22 @@ import Image from 'next/image';
 
 type ButtonProps = {
   type: 'button' | 'submit';
-  title: string;
+  title?: string;
   icon?: string;
-  variant: string;
+  iconTitle?: string;
+  variant?: string;
   full?: boolean;
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
-const Button = ({ type, title, icon, variant, full, onClick }: ButtonProps) => {
+const Button = ({ type, title, icon, iconTitle, variant, full, onClick }: ButtonProps) => {
   return (
     <button
       className={`flexCenter gap-3 rounded-full ${variant} ${full && 'w-full'}`}
       type={type}
       onClick={onClick}
     >
-      {icon && <Image src={icon} alt={title} width={16} height={16} loading='lazy' />}
+      {icon && <Image src={icon} alt={iconTitle || 'icon'} width={16} height={16} loading='lazy' />}
       <label className='text-base whitespace-nowrap cursor-pointer'>{title}</label>
     </button>
   );
