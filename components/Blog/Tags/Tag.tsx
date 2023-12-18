de4a34ad9a3bed18b25bdeeaf18812b426eae74c
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2023-12-19 00:10:45 +0200

    fixed value for disabled colors

--- components/Blog/Tags/Tag.tsx
+++ components/Blog/Tags/Tag.tsx
@@ -4,8 +4,7 @@ import { getCategoryColors } from '@/constants/colors';
 import { ITag } from '@/shared/types';
 
 const Tag = ({ tag, disabled, selected = false }: ITag) => {
-  const disabledColor: string =
-    'opacity-50 bg-light-primary/10 dark:bg-dark-primary/10 dark:text-dark-primary/50 cursor-not-allowed';
+  const disabledColor: string = 'opacity-50 cursor-not-allowed';
 
   const [defaultColor, selectedColor]: [string, string] = getCategoryColors(tag);
 
