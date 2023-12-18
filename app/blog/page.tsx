259eb9cb674905a97c8f3da105ac5e8c6980ac0a
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2023-12-19 00:19:24 +0200

    Create Blog page to showcase blog posts

---
+++ app/blog/page.tsx
@@ -0,0 +1,17 @@
+import Posts from '@/components/Blog/Posts';
+import Container from '@/components/Container';
+import PageWrapper from '@/components/PageWrapper';
+
+const Blog = () => {
+  return (
+    <PageWrapper>
+      <Container>
+        <section className='flex flex-col min-h-screen sm:col-span-5 md:col-span-4 items-center p-2 gap-16 mb-10'>
+          <Posts />
+        </section>
+      </Container>
+    </PageWrapper>
+  );
+};
+
+export default Blog;
